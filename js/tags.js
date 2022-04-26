d52eb24bcfc0ff6e73c036c215bca6ff60c0c59a
tayhaowen <82864387+tayhaowen@users.noreply.github.com>
2022-04-26 16:43:16 +0800

    Add files via upload

---
+++ js/tags.js
@@ -0,0 +1,13 @@
+var searchTags = [
+    "HTML",
+    "CSS",
+    "PHP",
+    "JavaScript",
+    "Python",
+    "Java",
+    "iOS",
+    "Swift",
+    "React",
+    "Web Design",
+    "Web Development"
+];
